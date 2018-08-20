a90acf9792bab93c573c17f3531ffed62230a240
Sofiane Medjkoune <sofiane@medjkoune.fr>
2018-08-20 20:24:51 +0200

    Update script to use the http_password

--- scripts/state_off.sh
+++ scripts/state_off.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-curl -X POST -d '{"state": "OFF"}' http://127.0.0.1:8123/api/states/$1
+SCRIPT_PATH=$(dirname $0)
+HASS_PASSWORD=$(grep 'http_password:' ${SCRIPT_PATH}/../secrets.yaml | awk '{print $2}')
+
+curl -H "x-ha-access: ${HASS_PASSWORD}" -X POST -d '{"state": "OFF"}' http://127.0.0.1:8123/api/states/$1
