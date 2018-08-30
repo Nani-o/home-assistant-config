af907534ca69502f9cff0bce219a77496d64d243
Sofiane Medjkoune <sofiane@medjkoune.fr>
2018-08-31 00:53:39 +0200

    Add the plex_scan script

---
+++ scripts/plex_scan.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from plexapi.server import PlexServer
+import yaml
+import os
+import sys
+
+script_path = os.path.dirname(os.path.realpath(sys.argv[0]))
+
+with open('{}/../travis_secrets.yaml'.format(script_path)) as file:
+    secrets = yaml.load(file)
+
+plex_ip = secrets['plex_ip']
+token = secrets['plex_token']
+baseurl = 'http://{}:32400'.format(plex_ip)
+plex = PlexServer(baseurl, token)
+
+plex.library.update()
