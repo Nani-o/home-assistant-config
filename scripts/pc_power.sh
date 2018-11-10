a511462c44b5cc02347c88e99a44bd623707a43d
Sofiane Medjkoune <sofiane@medjkoune.fr>
2018-11-10 01:41:00 +0100

    Fixes   * Syntax error and typos in script pc_power   * Change script mode to 0755   * Put back consider home value to 15 for device trackers

--- scripts/pc_power.sh
+++ scripts/pc_power.sh
@@ -6,7 +6,8 @@ if [[ "$1" == "on" ]]
 then
     PC_MAC=$(grep 'pc_mac:' ${SCRIPT_PATH}/../secrets.yaml | awk '{print $2}')
     wakeonlan "$PC_MAC"
-elif [[ "$1" == "on" ]];
+elif [[ "$1" == "off" ]]
+then
     PC_IP=$(grep 'pc_ip:' ${SCRIPT_PATH}/../secrets.yaml | awk '{print $2}')
-    ssh -t nani@$PC_IP "shutdown /s /t 0"
+    ssh nani@$PC_IP "shutdown /s /t 0"
 fi
